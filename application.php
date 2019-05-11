43092fc57feb58755ba8a0d3d6c44ce3e2837d2e
James Greenleaf <james@greenle.af>
2019-05-11 16:39:18 -0400

    Set default timezone for this script to New York - Got sick of this being reset to UTC by osx or homebrew in php.ini

--- application.php
+++ application.php
@@ -6,6 +6,8 @@ use aMoniker\LedgerPrices\Command\UpdatePricesCommand;
 use aMoniker\LedgerPrices\Command\DisplayPricesCommand;
 use Symfony\Component\Console\Application;
 
+date_default_timezone_set('America/New_York');
+
 $app = new Application();
 $app->addCommands([
     new UpdatePricesCommand(),
